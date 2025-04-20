814c264f151c2d35fc736ba7e5a2444c3758356a
sito schwarzenberger <sitoschwarzenberger@sitos-Mac-mini.local>
2025-04-20 17:45:50 +0200

    sql variable set on render

--- db.js
+++ db.js
@@ -1,13 +1,9 @@
-const mysql = require('mysql2/promise');
-
 const pool = mysql.createPool({
-  host: 'database-5017698764.ud-webspace.de', // Replace with your database host from united-domains.de
-  user: 'dbu349972',                // Replace with your DB-User
-  password: 'SitoformDB+25',          // Replace with your DB-Passwort
-  database: 'dbs14148917',              // Replace with your database name
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool;
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
