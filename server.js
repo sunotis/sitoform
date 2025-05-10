f37e1d391780545f08c0653115c7618d2e5d0685
sito schwarzenberger <sitoschwarzenberger@sitos-Mac-mini.local>
2025-05-10 22:53:14 +0200

    Hybrid YouTube handling

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.youtube.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://sitoform.com https://via.placeholder.com; frame-src 'self' https://www.youtube.com; connect-src 'self' https://sitoform25.onrender.com https://ydfkrwjafnuvdvezpkcp.supabase.co"
+    "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.youtube.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://sitoform.com https://via.placeholder.com https://img.youtube.com blob:; frame-src 'self' https://www.youtube.com https://player.vimeo.com; connect-src 'self' https://sitoform25.onrender.com https://ydfkrwjafnuvdvezpkcp.supabase.co https://www.youtube.com https://player.vimeo.com"
   );
+  res.setHeader('Permissions-Policy', "compute-pressure=(self 'https://www.youtube.com')");
   next();
 });
 
